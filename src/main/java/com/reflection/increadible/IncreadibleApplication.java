5689fbf8ba5bca24a72e43702eb5606a033326ef
judycodes <43575215+judycodes@users.noreply.github.com>
2019-11-06 12:45:46 -0500

    user model created

---
+++ src/main/java/com/reflection/increadible/IncreadibleApplication.java
@@ -0,0 +1,13 @@
+package com.reflection.increadible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncreadibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncreadibleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/reflection/increadible/model/User.java
@@ -0,0 +1,44 @@
+package com.reflection.increadible.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+
+@Entity
+@Table(name= "users")
+public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String goal;
+
+    public User() {}
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getUsername() {return username;}
+
+    public void setUsername(String username) {this.username = username;}
+
+    public void setPassword(String password) {this.password = password;}
+
+//    public String getGoal() {return goal;}
+//
+//    public void setGoal(String goal) {this.goal = goal;}
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public String getPassword() {return password;}
+
+}
