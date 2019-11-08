c656679d98892a264bb187f8746c229003346d1a
judycodes <43575215+judycodes@users.noreply.github.com>
2019-11-08 14:07:07 -0500

    landing design v2

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,17 +5,13 @@ const Landing = () => {
         <div id= "landing-container">
             <div id="landing-content">
               <div id="landing-nav">
-              <h2 id="landing-title">incREADible</h2>
+
               <button>sign in</button>
               </div>
 
               <div id="landing-tagline">
-              <blockquote>
-              <h1 className="quotation">we do not learn from experience ... we learn from reflecting on experience.
-              </h1>
-              <p id="landing-quote-author">John Dewey</p>
-              </blockquote>
-              <button>Get Started</button>
+              <h1 id="landing-title">INCREADIBLE</h1>
+              <button id="get_started_btn">Get Started</button>
               </div>
 
             </div>
@@ -25,3 +21,10 @@ const Landing = () => {
 
 
 export default Landing;
+
+//Move to about
+// <blockquote>
+// <h1 className="quotation">we do not learn from experience ... we learn from reflecting on experience.
+// </h1>
+// <p id="landing-quote-author">John Dewey</p>
+// </blockquote>
