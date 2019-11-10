e3cab6e78d2930ab8d46bbbbc457b124e54d9187
judycodes <43575215+judycodes@users.noreply.github.com>
2019-11-10 12:56:46 -0500

    result content renders on page

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 
+//import custom Component
+import Navbar from './Navbar';
+import SearchResult from './SearchResult';
+
 class Search extends Component{
   constructor(props){
     super(props);
 
     this.state = {
       searchInput : ' ',
-      searchResponses : []
+      searchResponses : [],
+
+      //error handling
+      searchResFetchSuccess: false,
+      searchResFetchError: false
     }
   }
 
@@ -23,6 +31,7 @@ handleSearchSubmit = (e) => {
 
   if(!this.state.searchInput) alert("Please provide valid input.");
 
+  //search params needed by api
   let params = {
     action: 'query',
     list: 'search',
@@ -30,65 +39,73 @@ handleSearchSubmit = (e) => {
     format: 'json'
   }
 
+  //formats the wikiUrl for api call with input given
   wikiUrl += '?origin=*';
   Object.keys(params).forEach((key) => {
     wikiUrl += "&" + key + "=" + params[key];
   });
 
+try {
   fetch(wikiUrl)
+
       .then( res => {
           return res.json();
         })
-      //the following when uncommented causes uncaught promise for below
-      // .then(res => {
-      //   console.log(res, "res from wiki api call");
-      //   console.log(res.query.search, "res.query.search");
-      // })
 
       //grabs specific pieces of res data for each result
       .then( res => {
 
-        res.query.search.map( foundResult => {
-          return this.state.searchResponses.push({
-            pageUrl: 'no-link',
-            pageId: foundResult.pageid,
-            pageTitle: foundResult.title,
-            pageSnippet: foundResult.snippet
+        if(res.query.search) { //if api call yields results, save results contentt in state
+          res.query.search.map( foundResult => {
+            return this.state.searchResponses.push({
+              pageUrl: 'no-link',
+              pageId: foundResult.pageid,
+              pageTitle: foundResult.title,
+              pageSnippet: foundResult.snippet
+            })
           })
-        })
 
-        console.log(this.state.searchResponses, "searchResponses");
-        this.handleUrlFetch();
+          console.log(this.state.searchResponses, "searchResponses");
+          this.handleUrlFetch();
+
+          this.setState({
+            searchResFetchSuccess: true
+          })
+        } else {
+          //todo: change to paragraph display
+          return alert("No results found. Try another search.");
+
+        }
       })
 
-      //TODO - clear search input
-      // this.setState({
-      //   searchInput: ""
-      // })
+} catch(error) {
+  console.log(`Error from search responses fetch: ${error}`);
+
+    this.setState({
+      searchResFetchError: true
+    })
+  }
 
 }
 
+//finds and sets full url path for each result in the result object of the searchResponses array
 handleUrlFetch = () => {
 
   this.state.searchResponses.map( specificPage => {
 
     let pathToRetrieveFullUrl = `https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=info&pageids=${specificPage.pageId}&inprop=url&format=json`;
 
-    fetch(pathToRetrieveFullUrl)
+    return fetch(pathToRetrieveFullUrl)
 
     .then(res => {
       return res.json();
     })
 
     .then( res => {
-
-        return specificPage.pageUrl = res.query.pages[specificPage.pageId].fullurl;
-
+      return specificPage.pageUrl = res.query.pages[specificPage.pageId].fullurl;
     })
 
   })
-
-  console.log(this.state.searchResponses, "searchResponses with url");
 
 }
 
@@ -98,7 +115,6 @@ handleSearchInput = (e) => {
 
   //trims leading and/or trailing whitespaces and then matches all whitespaces between words in input and replaces them with '%20'
  let noWhiteSpaceInput = e.target.value.trim().replace(/\s/g,'%20');
-
 
   this.setState({
     searchInput : noWhiteSpaceInput
@@ -110,11 +126,26 @@ handleSearchInput = (e) => {
 
     //stores search results - api response
     let searchResultsContent = [];
+    // console.log(searchResultsContent, "searchResultsContent");
+
+    searchResultsContent.push(this.state.searchResponses.map((specificResult, index) => {
+
+      return <SearchResult
+              key={index}
+              url={specificResult.pageUrl}
+              resultId={specificResult.pageId}
+              title={specificResult.pageTitle}
+              snippet={specificResult.pageSnippet}
+              />;
+
+    }));
 
     return(
-      <div>
+      <div id="search-container">
+      <Navbar />
+      <div id="search-content">
 
-        <h1 id="search-title">The Unending Search For Knowledge</h1>
+        <h1 id="search-title">Search For Knowledge</h1>
 
       <form>
 
@@ -125,8 +156,9 @@ handleSearchInput = (e) => {
           placeholder='Curious about ...' />
 
         <button
+          id="search_btn"
           type='submit'
-          onClick={this.handleSearchSubmit}>Enlighten Me</button>
+          onClick={this.handleSearchSubmit}>enlighten me</button>
 
         <a
           href="https://en.wikipedia.org/wiki/Special:Random"
@@ -138,6 +170,7 @@ handleSearchInput = (e) => {
 
       {searchResultsContent}
 
+      </div>
       </div>
     )
   }
---
+++ client/src/components/SearchResult.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchResult = (props) => {
+  return (
+    <div>
+    {props.title}
+    </div>
+  )
+}
+
+export default SearchResult;
