d93799a6ded97921bf1dd02d7700c3f2953afafb
judycodes <43575215+judycodes@users.noreply.github.com>
2019-11-10 21:24:56 -0500

    flyway and sql file added

---
+++ increadible-api/src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    goal VARCHAR(255) NULL
+);
+
+CREATE TABLE reflections (
+    reflection_id SERIAL PRIMARY KEY,
+    subject VARCHAR(255) NOT NULL,
+    tidbit VARCHAR(255) NOT NULL,
+    user_id BIGINT NOT NULL
+
+);
+
+ALTER TABLE reflections ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id);
+
