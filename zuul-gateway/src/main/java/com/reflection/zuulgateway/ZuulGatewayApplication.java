ae9e61c5f341191500f332a44fe2c63cb2626fee
judycodes <43575215+judycodes@users.noreply.github.com>
2019-11-10 21:14:44 -0500

    docker-compose yml created, zuul and increadible api configured

--- zuul-gateway/src/main/java/com/reflection/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/reflection/zuulgateway/ZuulGatewayApplication.java
@@ -2,8 +2,12 @@ package com.reflection.zuulgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
 public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
