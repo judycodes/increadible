3438939f83fc2bfb28507d5316930beffaaf8a75
judycodes <43575215+judycodes@users.noreply.github.com>
2019-11-10 20:57:46 -0500

    added eureka and zuul starter files, setup eureka with clean and install done

---
+++ zuul-gateway/src/main/java/com/reflection/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.reflection.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+}
---
+++ zuul-gateway/src/test/java/com/reflection/zuulgateway/ZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reflection.zuulgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
