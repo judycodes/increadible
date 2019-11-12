b17a9e41aa31d77c8eb0b2c5e2eb2fb19b367e6c
judycodes <43575215+judycodes@users.noreply.github.com>
2019-11-12 05:51:59 -0500

    random fact renders from fetch

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,19 +3,47 @@ import React, { Component } from 'react';
 //custom components
 import Navbar from './Navbar';
 
-
 class Home extends Component {
   constructor(props){
     super(props);
 
     this.state = {
+      randomFactFetchSuccess: false,
+      randomFactFetchError: false,
+      randomFact: ''
 
     }
   }
 
   componentDidMount(){
 
+    try{
+
+     fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+
+      .then( res => {
+        return res.json();
+      })
+
+      .then(res => {
+        console.log(res.text, "random fact");
+
+        if(res.text !== ''){
+          this.setState({
+            randomFactFetchSuccess: !this.state.randomFactFetchSuccess,
+            randomFact: res.text
+          })
+        }
+
+      })
+    } catch(error) {
+    console.log(`Random fact fetch error: ${error}`);
+
+    this.setState({
+      randomFactFetchError: !this.state.randomFactFetchError
+    })
   }
+}
 
   render(){
     return(
@@ -23,7 +51,7 @@ class Home extends Component {
       <div id="home_container">
 
       <Navbar />
-
+      {this.state.randomFact ? <p>{this.state.randomFact}</p> : 'Random Fact Loading...'}
 
 
       </div>
